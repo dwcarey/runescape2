124ca6462dbcea5fd812a64ab7ede405f56cab80
dwcarey <127503496+dwcarey@users.noreply.github.com>
2024-05-27 17:51:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>can't tell me nothin</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {count} mo fuggaz
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          and they aint sayin nothin
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        bitches aint shit
       </p>
     </>
   )
